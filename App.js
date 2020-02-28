71cc53073ce9ace81c4d07311303a66bf07eb5aa
Shehbaz Jafri <shehbaz@buildops.com>
2020-02-28 15:18:00 +0530

    link netinfo, update app with datastore

--- App.js
+++ App.js
@@ -1,113 +1,133 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
+ * React Native DataStore Sample App
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, {Component} from 'react';
+import {Text, AsyncStorage, StyleSheet, ScrollView} from 'react-native';
+global.Buffer = global.Buffer || require('buffer').Buffer;
+import Amplify from '@aws-amplify/core';
+import {DataStore, Predicates} from '@aws-amplify/datastore';
+import {Post, PostStatus, Comment} from './src/models';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import awsConfig from './aws-exports';
+Amplify.configure(awsConfig);
+let subscription;
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
+  }
+
+  componentDidMount() {
+    this.onQuery();
+    subscription = DataStore.observe(Post).subscribe(msg => {
+      console.log('SUBSCRIPTION_UPDATE', msg);
+      this.onQuery();
+    });
+  }
+
+  componentWillUnmount() {
+    subscription.unsubscribe();
+  }
+
+  onCreatePost() {
+    DataStore.save(
+      new Post({
+        title: `New Post ${Date.now()}`,
+        rating: (function getRandomInt(min, max) {
+          min = Math.ceil(min);
+          max = Math.floor(max);
+          return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+        })(5, 10),
+        status: PostStatus.ACTIVE,
+      }),
+    );
+  }
+
+  async onCreatePostAndComments() {
+    const post = new Post({
+      title: `New Post with comments ${Date.now()}`,
+      rating: 5,
+      status: PostStatus.ACTIVE,
+    });
+
+    await DataStore.save(post);
+
+    for (let i = 0; i < 2; i++) {
+      DataStore.save(
+        new Comment({
+          content: `New comment ${Date.now()}`,
+          post,
+        }),
+      );
+    }
+  }
+
+  onQuery = async () => {
+    const posts = await DataStore.query(Post, c => c.rating('gt', 2));
+    console.log('QUERY_POSTS_RESULT', posts);
+    const comments = await DataStore.query(Comment);
+    this.setState({posts});
+    console.log('QUERY_COMMENTS_RESULT', comments);
+  };
+
+  onDelete = async () => {
+    const deletedPosts = await DataStore.delete(Post, Predicates.ALL);
+    console.log('DELETE_RESULT', deletedPosts);
+  };
+
+  getAsyncStorage = async () => {
+    const allKeys = await AsyncStorage.getAllKeys();
+    const allDataStoreKeys = allKeys.filter(key =>
+      key.startsWith('@AmplifyDatastore'),
+    );
+    const allItems = await AsyncStorage.multiGet(allDataStoreKeys);
+    const records = allItems.map(([key, value]) => [key, JSON.parse(value)]);
+    console.log('ALL_STORE_RECORDS', records);
+  };
+
+  render() {
+    return (
+      <ScrollView
+        style={{paddingTop: 40, flex: 1}}
+        contentContainerStyle={{alignItems: 'center'}}>
+        <Text style={styles.text} onPress={this.getAsyncStorage}>
+          Get Store
+        </Text>
+        <Text style={styles.text} onPress={this.onCreatePost}>
+          Create One Post
+        </Text>
+        <Text style={styles.text} onPress={this.onCreatePostAndComments}>
+          Create Post & Comments
+        </Text>
+        <Text style={styles.text} onPress={this.onQuery}>
+          Query Posts
+        </Text>
+        <Text style={styles.text} onPress={this.onDelete}>
+          Delete Posts
+        </Text>
+        {this.state.posts.map((post, i) => (
+          <Text key={i}>{`${post.title} ${post.rating}`}</Text>
+        ))}
+      </ScrollView>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
 });
 
